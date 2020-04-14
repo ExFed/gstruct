94be46307d31a3efa37d0c10dd60be5367422332
David Allen <david.allen@columnzero.com>
2020-04-13 20:20:59 -0400

    Add Path

---
+++ subprojects/utils/src/main/java/com/columnzero/gstruct/util/Path.java
@@ -0,0 +1,154 @@
+package com.columnzero.gstruct.util;
+
+import java.util.*;
+
+public class Path<T> implements Iterable<T> {
+
+    private static final Path<Object> ROOT = new Path<>(new RootValue());
+
+    /**
+     * Gets the global root node.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> Path<T> getRoot() {
+        return (Path<T>) ROOT;
+    }
+
+    /**
+     * Creates a path that forms part of the global tree.
+     *
+     * @param elements Elements of the path. The first element is attached to the global root, the last element becomes
+     *                 a leaf.
+     * @param <T>      Type of elements within the path.
+     *
+     * @return A new path.
+     */
+    @SafeVarargs
+    public static <T> Path<T> of(T... elements) {
+        return of(Arrays.asList(elements));
+    }
+
+    /**
+     * Creates a path that forms part of the global tree.
+     *
+     * @param elements Elements of the path. The first element is attached to the global root, the last element becomes
+     *                 a leaf.
+     * @param <T>      Type of elements within the path.
+     *
+     * @return A new path.
+     */
+    public static <T> Path<T> of(Iterable<T> elements) {
+        Path<T> result = getRoot();
+        for (T element : elements) {
+            result = result.child(element);
+        }
+        return result;
+    }
+
+    private final T value;
+    private final Path<T> parent;
+    private final int depth;
+
+    private Path(T value) {
+        this(value, null);
+    }
+
+    private Path(T value, Path<T> parent) {
+        this.value = Objects.requireNonNull(value);
+        this.parent = parent;
+        this.depth = parent != null ? parent.depth + 1 : 0;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public Path<T> getParent() {
+        return parent;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    /**
+     * Creates a path whose parent is this path instance.
+     *
+     * @param value Value of the new path node.
+     *
+     * @return A new path.
+     */
+    public Path<T> child(T value) {
+        return new Path<>(value, this);
+    }
+
+    /**
+     * Generates a list representation of this path. The first element is the base, the last element is the leaf.
+     *
+     * @return A list of the path elements.
+     */
+    public List<T> asList() {
+        return asListRecurse(new ArrayList<>(depth));
+    }
+
+    private List<T> asListRecurse(List<T> result) {
+        // traverse the parent if it exists
+        if (this.parent != null) {
+            parent.asListRecurse(result);
+        }
+
+        // should not emit the global root node
+        if (this != ROOT) {
+            // post-order insertion...
+            result.add(this.value);
+        }
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Path<?> path = (Path<?>) o;
+        return value.equals(path.value)
+                && depth == path.depth
+                && Objects.equals(parent, path.parent);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, parent);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return asList().iterator();
+    }
+
+    private StringJoiner buildString(StringJoiner sj) {
+        if (parent != null) {
+            parent.buildString(sj);
+        }
+        return sj.add(value.toString());
+    }
+
+    public String toString(CharSequence separator) {
+        return buildString(new StringJoiner(separator)).toString();
+    }
+
+    @Override
+    public String toString() {
+        return toString("/");
+    }
+
+    /**
+     * The global root node value.
+     */
+    private static final class RootValue {
+        @Override
+        public String toString() {
+            return "";
+        }
+    }
+}
---
+++ subprojects/utils/src/test/java/com/columnzero/gstruct/util/TestUtil.java
@@ -0,0 +1,49 @@
+package com.columnzero.gstruct.util;
+
+import com.google.common.truth.Truth;
+import org.junit.jupiter.api.function.Executable;
+
+import java.util.stream.Stream;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class TestUtil {
+    private TestUtil() {
+        throw new UnsupportedOperationException("utility");
+    }
+
+    @SuppressWarnings({"ConstantConditions", "EqualsWithItself"})
+    public static <T> void assertEqualsAndHashCode(T a1, T a2, T b) {
+        final Stream.Builder<Executable> exec = Stream.builder();
+
+        // null
+        exec.add(() -> assertThat(a1.equals(null)).isFalse())
+            .add(() -> assertThat(a2.equals(null)).isFalse())
+            .add(() -> assertThat(b.equals(null)).isFalse());
+
+        // reflexive
+        exec.add(() -> assertThat(a1.equals(a1)).isTrue());
+
+        // symmetric
+        exec.add(() -> assertThat(a1.equals(a2)).isTrue())
+            .add(() -> assertThat(a2.equals(a1)).isTrue())
+            .add(() -> assertThat(b.equals(a1)).isFalse())
+            .add(() -> assertThat(a1.equals(b)).isFalse());
+
+        // consistent
+        for (int i = 0; i < 10; i++) {
+            exec.add(() -> assertThat(a1.equals(a2)).isTrue())
+                .add(() -> assertThat(a1.equals(b)).isFalse());
+        }
+
+        // hashcode
+        final int a1Code = a1.hashCode();
+        final int a2Code = a2.hashCode();
+        exec.add(() -> assertThat(a1Code).isEqualTo(a2Code))
+            .add(() -> assertThat(a1.hashCode()).isEqualTo(a1Code))
+            .add(() -> assertThat(b.hashCode()).isNotEqualTo(a1Code));
+
+        assertAll(exec.build());
+    }
+}
