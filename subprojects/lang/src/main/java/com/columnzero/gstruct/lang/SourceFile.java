c515bfd9e3b95aba647f0a7e8675f07a4ebedcc8
David Allen <david.allen@columnzero.com>
2020-10-31 22:06:49 -0400

    Add SourceFile

---
+++ subprojects/lang/src/main/java/com/columnzero/gstruct/lang/SourceFile.java
@@ -0,0 +1,36 @@
+package com.columnzero.gstruct.lang;
+
+import com.columnzero.gstruct.util.Path;
+import com.columnzero.gstruct.util.Paths;
+import lombok.NonNull;
+import lombok.Value;
+
+import java.io.File;
+
+/**
+ * Represents a source file.
+ */
+@Value
+public class SourceFile {
+
+    @NonNull File file;
+
+    public SourceFile(File file) {
+        if (!file.isFile()) {
+            throw new IllegalArgumentException("not a regular file: " + file);
+        }
+
+        this.file = file;
+    }
+
+    /**
+     * Gets the namespace of this source file relative to a given root directory.
+     *
+     * @param rootDir Root of the file tree containing this source file.
+     *
+     * @return The namespace of the file.
+     */
+    public Path<String> getNamespace(File rootDir) {
+        return Paths.from(getFile().getParentFile(), rootDir);
+    }
+}
